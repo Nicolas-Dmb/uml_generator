471bc44340098313c7b7b850020644a636c9705e
Nicolas_dmb <gyfeix@gmail.com>
2025-04-04 10:23:17 +0200

    instance et classe faite il reste à récupérer les méthodes

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import os
 import sys
 from pathlib import Path
 from typing import Iterable, Any
-from tree_sitter import Language, Parser
+from tree_sitter import Language, Node, Parser
 import tree_sitter_javascript as javascript
+import tree_sitter_typescript as typescript
 JAVASCRIPT = Language(javascript.language())
-parser = Parser(JAVASCRIPT)
+TSX = Language(typescript.language_tsx())
+TS = Language(typescript.language_typescript())
+
 
 
 @dataclass
@@ -38,28 +41,32 @@ class Class():
     path:Path
     name:str
     code: str
+    node: Node
     method: Iterable["Method"] = field(default_factory=list)
     instance: Iterable["Instance"] = field(default_factory=list)
     children: Iterable["Dependance"] = field(default_factory=list) # type: ignore
 
     @classmethod
-    def register(cls, node:Any,link:Path, code:str)->'Class': # type: ignore
+    def register(cls, node:Node,link:Path, code:str)->'Class': # type: ignore
         name = ''
-        for child in node.children:
-            if child.type in ("type_identifier", "identifier", "name"):
-                name = code[child.start_byte:child.end_byte]
-                break
+        name_node = node.child_by_field_name("name")
+        if name_node:
+            name = code[name_node.start_byte:name_node.end_byte].decode("utf-8")
         return Class(
             class_type=node.type,
             path=link,
             name=name,
             code=code,
+            node=node,
         )
 
     def addMethod(self, method:Method)->'Class':
         return replace(self,method=self.method + [method])
     
-    def addInstance(self, instance:Instance)->'Class':
+    def addInstance(self,name:Node, type:Node)->'Class':
+        nameString = self.code[name.start_byte:name.end_byte].decode("utf-8")
+        typeString = self.code[type.start_byte:type.end_byte].decode("utf-8")
+        instance = Instance(name=nameString, type=typeString)
         return replace(self,instance=self.instance + [instance])
     
     def addchildren(self, children:Dependance)->'Class':
@@ -78,11 +85,24 @@ class FileScanner():
         if node.parent and node.parent.type == "program":
             return True
         return False
-    
+    @classmethod 
+    def checkStyleSheet(cls, node:Any,link:Path,code:str)->Class|None:
+        value_node = node.child_by_field_name("value")
+        if value_node and value_node.type == "call_expression":
+            func_node = value_node.child_by_field_name("function")
+            if func_node and func_node.type == "member_expression":
+                object_node = func_node.child_by_field_name("object")
+                object_text = code[object_node.start_byte:object_node.end_byte].decode("utf-8") if object_node else ""
+                if object_text == "StyleSheet":
+                    return
+        return Class.register(node=node, link=link,code=code)
     @classmethod
     def checkLexicalDeclaration(cls, node:Any,link:Path,code:str)->Class|None:
         for declarator in node.children:
             if declarator.type == "variable_declarator":
+                name_node = declarator.child_by_field_name("name")
+                if name_node: 
+                    return cls.checkStyleSheet(node=declarator,link=link,code=code)
                 value = declarator.child_by_field_name("value")
                 if value and value.type in ("arrow_function", "function"):
                     return Class.register(node=value, link=link,code=code)
@@ -104,7 +124,7 @@ class FileScanner():
 
             elif child.type == "export_statement":
                 for sub in child.children:
-                    if sub.type in ("function_declaration", "class_declaration"):
+                    if sub.type in ("function_declaration", "class_declaration","abstract_class_declaration"):
                         top_level_nodes.append(Class.register(node=sub, link=link,code=code))
                     elif sub.type == "lexical_declaration":
                         result = cls.checkLexicalDeclaration(node=sub,link=link,code=code)
@@ -115,33 +135,73 @@ class FileScanner():
 
         return top_level_nodes
     @classmethod
-    def searchMethod(cls)->Iterable[Method]:
+    def searchMethod(cls, classe:Class)->Class:
+        classe = classe
+        class_body = classe.node.child_by_field_name("body")
+        if not class_body : 
+            return classe
+        for child in class_body.children:
+            if child.type == "abstract_method_signature":
+                name = child.child_by_field_name("name")
+                args = child.child_by_field_name("parameters")
+                return_type = child.child_by_field_name("return_type")
+                
         pass
     @classmethod
-    def searchInstance(cls)->Iterable[Instance]:#"lexical_declaration"
-        pass
+    def searchInstance(cls, classe:Class)->Class:#"lexical_declaration"
+        classe = classe
+        class_body = classe.node.child_by_field_name("body")
+        if not class_body : 
+            return classe
+        for child in class_body.children:
+            if child.type == "public_field_definition":
+                name = child.child_by_field_name("name")
+                type = child.child_by_field_name("type")
+                if name and type : 
+                    classe = classe.addInstance(name=name,type=type)
+            if child.type == "lexical_declaration":
+                for declarator in child.children:
+                    if declarator.type == "variable_declarator":
+                        name = declarator.child_by_field_name("name")
+                        type = declarator.child_by_field_name("value")
+                        if name and type : 
+                            if type.type not in ("arrow_function", "function"): 
+                                classe = classe.addInstance(name=name,type=type)
+            #if child.type == "abstract_method_signature":
+        return classe
+        
     @classmethod
     def searchDependancies(cls)->Iterable[Dependance]: #"export_statement"
         pass
     @classmethod
-    def fileScanner(cls, link:Path, project:Project)->Project:
+    def fileScanner(cls, link:Path, project:Project, parser:Parser)->Project:
         contenu = link.read_text(encoding="utf-8")
-        tree = parser.parse(contenu.encode(encoding="utf8"))
+        code_bytes = contenu.encode("utf-8")
+        tree = parser.parse(code_bytes)
         root = tree.root_node
-        classs = cls.searchClass(root=root,link=link, code=contenu)
-        print(str(link)+': \n')
+        classs = cls.searchClass(root=root,link=link, code=code_bytes)
         for classe in classs:
-            print(f'{classe.class_type} : {classe.name}')
+            Newclasse = cls.searchInstance(classe)
+            if Newclasse:
+                print(link,'\n')
+                print(f"🗞️ classe : {Newclasse.name} : {Newclasse.instance}")
+            else:
+                print(f"🚨 erreur sur la classe {classe.name} / {classe.path}")
 
 
 class NavigateTroughtProject():
     def registerFile(self, link:Path, fileName:str, project:Project)->Project|None:
         if 'test' in fileName:
             return
-        if fileName.endswith((".ts", ".tsx", ".js")):
-            project = FileScanner.fileScanner(link=link,project=project)
+        if fileName.endswith((".tsx",)):
+            parser = Parser(TSX)
+            project = FileScanner.fileScanner(link=link,project=project,parser=parser)
+        elif fileName.endswith((".ts")):
+            parser = Parser(TS)
+            project = FileScanner.fileScanner(link=link,project=project,parser=parser)
         elif fileName.endswith((".js")):
-            project = FileScanner.fileScanner(link=link,project=project)
+            parser = Parser(JAVASCRIPT)
+            project = FileScanner.fileScanner(link=link,project=project, parser=parser)
         else:
             return
         return project
